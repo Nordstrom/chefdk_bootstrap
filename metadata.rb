65f3af09c2f504cf300ac4d3bd8f64743537eab5
Doug Ireton <doug.ireton@nordstrom.com>
2015-05-20 16:54:21 -0700

    Update metadata info. Patch bump

--- metadata.rb
+++ metadata.rb
@@ -6,9 +6,14 @@
 name             'chefdk_bootstrap'
 maintainer       'Nordstrom, Inc.'
 maintainer_email 'techcheftm@nordstrom.com'
-license          'apache2'
-description      'Installs/Configures chefdk_bootstrap'
-version          '0.1.0'
+license          'Apache 2.0'
+description      'Bootstrap a developer workstation for local Chef development using the ChefDK'
+version          '0.1.1'
+
+supports 'windows'
 
 depends 'chocolatey', '~> 0.3'
 depends 'git', '~> 4.2'
+
+source_url 'https://github.com/Nordstrom/chefdk_bootstrap'
+issues_url 'https://github.com/Nordstrom/chefdk_bootstrap/issues'
