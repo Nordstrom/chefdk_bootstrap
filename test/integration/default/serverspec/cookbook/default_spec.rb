2ca856617363a6d5fd93f574ef8117f469561eea
Doug Ireton <doug.ireton@nordstrom.com>
2016-01-06 09:42:14 -0800

    Release v1.4.1
    Install Vagrant via community Vagrant cookbook
    
    Fixes #48

--- test/integration/default/serverspec/cookbook/default_spec.rb
+++ test/integration/default/serverspec/cookbook/default_spec.rb
@@ -15,13 +15,6 @@
 
 require 'spec_helper'
 
-# this is a generic ServerSpec test.  It just runs 'whoami'
-# and confirms that it returns 'root'.  In a real test suite,
-# you would want to test things like directories, users,
-# processes, services, etc.
-#
-# for more information on the things ServerSpec can test, refer
-# to the documentation at http://serverspec.org
-describe command('whoami') do
-  its(:stdout) { should eq "root\n" }
+describe command('vagrant -v') do
+  its(:exit_status) { should eq(0) }
 end
